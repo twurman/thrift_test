5403893c37f2c078339924346d37d6cff08b649b
Tim W <twurman@umich.edu>
2015-07-01 16:40:49 -0400

    removed qa files

--- template/test-concurrency-qa.sh
+++
@@ -1,5 +0,0 @@
-# script to send multiple client requests in parallel
-./qaclient "what is the speed of light?" $1 &
-./qaclient "who is the President of the United States?" $1 &
-./qaclient "what is the speed of sound?" $1 &
-./qaclient "who wrote Harry Potter?" $1 &
