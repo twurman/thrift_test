db142add66f089f66d84e79ffadd1ac0b1d71a5e
Tim W <twurman@umich.edu>
2015-07-01 16:39:22 -0400

    classpath for running

--- template/start-qa.sh
+++ template/start-qa.sh
@@ -6,12 +6,15 @@
 
 # Add my classes to class path
 # (These class paths are only necessary when running the program.)
+thrift=~/thrift-0.9.2
+JAVA_CLASS_PATH=$thrift/lib/java/build/libthrift-0.9.2.jar:$thrift/lib/java/build/lib/slf4j-api-1.5.8.jar:$thrift/lib/java/build/lib/slf4j-log4j12-1.5.8.jar:$thrift/lib/java/build/lib/log4j-1.2.14.jar
+
 export JAVA_CLASS_PATH=$JAVA_CLASS_PATH:`pwd`:`pwd`/gen-java
 
 # Rather than forking a subshell, execute all commands
 # in java-config.sh in the current shell.
-cd ../common
-	source ./qa-runtime-config.sh
+# cd ../common
+# 	source ./qa-runtime-config.sh
 # NOTE: this script starts in ../common/question-answer
 
 # Use cp flag to avoid cluttering up the CLASSPATH environment variable
