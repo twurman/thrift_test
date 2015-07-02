5f3409bbf92dff0a0ead5b32b1cb99bf245af0f3
Tim W <twurman@umich.edu>
2015-07-02 17:17:50 -0400

    added simple version

--- template/compile-qa.sh
+++ template/compile-qa.sh
@@ -24,18 +24,10 @@ echo -e "./compile-qa.sh: Making QA client..."
 make
 printdivision
 
-# Compile server
-# 'source' command: Rather than forking a subshell, execute all commands
-# in the current shell.
-# cd ../common
-# 	./compile-openephyra.sh
-# 	printdivision
-# 	source ./qa-compile-config.sh
-# 	printdivision
-# cd ../template
 thrift=~/thrift-0.9.2
-JAVA_CLASS_PATH=$thrift/lib/java/build/libthrift-0.9.2.jar:$thrift/lib/java/build/lib/slf4j-api-1.5.8.jar:$thrift/lib/java/build/lib/slf4j-log4j12-1.5.8.jar:$thrift/lib/java/build/lib/log4j-1.2.14.jar:httpcomponents-core/lib/httpcore-4.2.3.jar
+JAVA_CLASS_PATH=$thrift/lib/java/build/libthrift-0.9.2.jar:$thrift/lib/java/build/lib/slf4j-api-1.5.8.jar:$thrift/lib/java/build/lib/slf4j-log4j12-1.5.8.jar:$thrift/lib/java/build/lib/log4j-1.2.14.jar
 
 # Use cp flag to avoid cluttering up the CLASSPATH environment variable
 echo -e "javac -cp $JAVA_CLASS_PATH QADaemon.java QAServiceHandler.java gen-java/qastubs/QAService.java\n\n"
 javac -cp $JAVA_CLASS_PATH -Xlint:unchecked -Xlint:deprecation QADaemon.java QAServiceHandler.java gen-java/qastubs/QAService.java
+javac -cp $JAVA_CLASS_PATH QADaemonSimple.java QAServiceHandler.java gen-java/qastubs/QAService.java
