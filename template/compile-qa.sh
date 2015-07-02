c2791d52abe0e9534f9ef7d1f2b56fe7b5f1ec43
Tim W <twurman@umich.edu>
2015-07-02 15:56:25 -0400

    older version of httpcore needed

--- template/compile-qa.sh
+++ template/compile-qa.sh
@@ -34,7 +34,7 @@ printdivision
 # 	printdivision
 # cd ../template
 thrift=~/thrift-0.9.2
-JAVA_CLASS_PATH=$thrift/lib/java/build/libthrift-0.9.2.jar:$thrift/lib/java/build/lib/slf4j-api-1.5.8.jar:$thrift/lib/java/build/lib/slf4j-log4j12-1.5.8.jar:$thrift/lib/java/build/lib/log4j-1.2.14.jar:httpcomponents-core-4.4.1/lib/httpcore-4.4.1.jar
+JAVA_CLASS_PATH=$thrift/lib/java/build/libthrift-0.9.2.jar:$thrift/lib/java/build/lib/slf4j-api-1.5.8.jar:$thrift/lib/java/build/lib/slf4j-log4j12-1.5.8.jar:$thrift/lib/java/build/lib/log4j-1.2.14.jar:httpcore-4.2.3.jar
 
 # Use cp flag to avoid cluttering up the CLASSPATH environment variable
 echo -e "javac -cp $JAVA_CLASS_PATH QADaemon.java QAServiceHandler.java gen-java/qastubs/QAService.java\n\n"
